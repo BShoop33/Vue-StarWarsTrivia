b02f21e17f26f8d8e9cecdb04b0e1483f1cd133a
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-05 11:04:39 -0600

    populating cards with data completed

--- src/components/triviaCards.vue
+++ src/components/triviaCards.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="triviaCardsContainer">
-    <div class="triviaCard"></div>
+    <div class="triviaCard">
+      {{ cardInfo.question }}
+      <button class="showAnswerButton">Show Answer</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["cardInfo"],
+};
 </script>
 
 <style scoped>
+.showAnswerButton {
+  font-size: 18pt;
+  margin-left: 1.25em;
+  width: 8em;
+  background-color: rgb(55, 243, 218);
+}
+
 .triviaCard {
   width: 12em;
-  height: 12em;
+  height: 8em;
   border: 5px;
   border-style: solid;
   border-color: green;
@@ -19,6 +31,10 @@ export default {};
   margin: 2em;
   font-size: 16pt;
   padding: 0.4em;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  overflow: auto;
 }
 
 .triviaCardsContainer {
